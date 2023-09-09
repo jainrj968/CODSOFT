4915462db8e8522a108142d1d38ab26a59063f03
jainrj968 <121357901+jainrj968@users.noreply.github.com>
2023-09-10 01:11:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            
+            input.value = string;
+        }
 
-const screen= ScrollReveal({ 
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-screen.reveal('.hero-text',{delay:200 , origin:'top'});
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
